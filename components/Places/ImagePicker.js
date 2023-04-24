f98bf8cf85834ee83f19f01dd0f4787bebcf80f2
Your Name <urielparavi75@gmail.com>
2023-04-24 19:59:43 +0300

    197

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,8 +1,39 @@
-import { Button, View } from 'react-native';
-import { launchCameraAsync } from 'expo-image-picker';
+import { Alert, Button, View } from 'react-native';
+import {
+  PermissionStatus,
+  launchCameraAsync,
+  useCameraPermissions,
+} from 'expo-image-picker';
 
 const ImagePicker = () => {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  const verifyPermissions = async () => {
+    // Means we don't know yet if we have access or not - if we have the permission to use the camera
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      // can be true or false
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient Permissions!',
+        'You need to grant camera permissions to use this app'
+      );
+      return false;
+    }
+    return true;
+  };
+
   const takeImageHandler = async () => {
+    const hasPermissionawait = await verifyPermissions();
+
+    if (!hasPermissionawait) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       // allow the user edit the photo before he send it
       allowsEditing: true,
