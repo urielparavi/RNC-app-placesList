20167160eddb9040e6ee139f9e7405e91da6da4c
Your Name <urielparavi75@gmail.com>
2023-04-28 19:39:54 +0300

    211

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,7 +1,18 @@
+import { useIsFocused } from '@react-navigation/native';
 import PlacesList from '../components/Places/PlacesList';
+import { useEffect, useState } from 'react';
 
-const AllPlaces = () => {
-  return <PlacesList />;
+const AllPlaces = ({ route }) => {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    if (isFocused && route.params) {
+      // place because in AllPlaces we defined the place params
+      setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, [isFocused, route]);
+
+  return <PlacesList places={loadedPlaces} />;
 };
 
 export default AllPlaces;
